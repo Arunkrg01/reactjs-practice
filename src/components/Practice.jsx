19ee7a8ef25bbceb244a950309dee23b87addc6a
arunkrg01 <arunkrg01@gmail.com>
2025-03-10 10:49:54 +0530

    Three files added

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,32 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
 function Practice(props) {
-    const [data, setData] = useState([]);
-    const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(5);
-
-    const fetchData = async () => {
-        let response = await fetch("https://dummyjson.com/users");
-        let result = await response.json();
-        let users = await result.users;
-        setData(users);
-        console.log("response", response);
-    };
+  
 
     useEffect(() => {
         fetchData();
     }, []);
     return (
         <div >
-            {
-                data?.map((item) => (
-                    <div key={item.id}>
-                        <p>{item.firstName}</p>
-                        <p>{item.email}</p>
-                        <img src={item.image} alt="nothing" />
-                    </div>
-                ))
-            }
+           hello
         </div>
     );
 }
